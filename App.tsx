bd48019671c15c9a96b2a74357175752e6d33eeb
shingo.sugure <gonshi555dashiyo@gmail.com>
2022-04-10 10:04:54 +0900

    Create Box

--- App.tsx
+++ App.tsx
@@ -1,11 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.itemContainer}>
+        <View style={styles.leftContainer}>
+          <Image
+            style={{ width: 100, height: 100 }}
+            source={{
+              uri: 'https://picsum.photos/500',
+            }}
+          />
+        </View>
+        <View style={styles.rightContainer}>
+          <Text numberOfLines={3} style={styles.text}>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Harum
+            nihil deserunt blanditiis quod delectus quaerat sunt aperiam
+            molestias esse corporis possimus odio reprehenderit commodi
+            assumenda, hic suscipit dicta ab quidem.
+          </Text>
+          <Text style={styles.subText}>React News</Text>
+        </View>
+      </View>
+      {/* <StatusBar style="auto" /> */}
     </View>
   );
 }
@@ -16,5 +34,27 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  itemContainer: {
+    height: 100,
+    width: '100%',
+    borderColor: 'gray',
+    borderWidth: 1,
+    flexDirection: 'row',
+  },
+  leftContainer: {
+    width: 100,
+  },
+  rightContainer: {
+    flex: 1,
+    padding: 10,
+    justifyContent: 'space-between',
+  },
+  text: {
+    fontSize: 16,
+  },
+  subText: {
+    fontSize: 12,
+    color: 'gray',
   },
 });
