200c58dde7be66950f59c0433598bf361f20a38c
shingo.sugure <gonshi555dashiyo@gmail.com>
2022-04-10 10:48:06 +0900

    Import ListItem Component

--- App.tsx
+++ App.tsx
@@ -1,30 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import { Image, StyleSheet, Text, View } from 'react-native';
+import { VFC } from 'react';
+import { FlatList, SafeAreaView, StyleSheet, View } from 'react-native';
+import ListItem from './components/ListItem';
+import articles from './dummy/articles.json';
+
+type Props = {
+  urlToImage: string;
+  title: string;
+  author: string;
+  publishedAt: string;
+}
+
+type ItemProps = {
+  item: Props
+}
 
 export default function App() {
+  const renderItem: VFC<ItemProps> = ({ item }) => (
+    <ListItem
+      imageUrl={item.urlToImage}
+      title={item.title}
+      author={item.author}
+    />
+  );
+
   return (
-    <View style={styles.container}>
-      <View style={styles.itemContainer}>
-        <View style={styles.leftContainer}>
-          <Image
-            style={{ width: 100, height: 100 }}
-            source={{
-              uri: 'https://picsum.photos/500',
-            }}
-          />
-        </View>
-        <View style={styles.rightContainer}>
-          <Text numberOfLines={3} style={styles.text}>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Harum
-            nihil deserunt blanditiis quod delectus quaerat sunt aperiam
-            molestias esse corporis possimus odio reprehenderit commodi
-            assumenda, hic suscipit dicta ab quidem.
-          </Text>
-          <Text style={styles.subText}>React News</Text>
-        </View>
-      </View>
-      {/* <StatusBar style="auto" /> */}
-    </View>
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={articles}
+        renderItem={renderItem}
+        keyExtractor={(item, index) => index.toString()}
+      />
+    </SafeAreaView>
   );
 }
 
@@ -32,29 +39,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  itemContainer: {
-    height: 100,
-    width: '100%',
-    borderColor: 'gray',
-    borderWidth: 1,
-    flexDirection: 'row',
-  },
-  leftContainer: {
-    width: 100,
-  },
-  rightContainer: {
-    flex: 1,
-    padding: 10,
-    justifyContent: 'space-between',
-  },
-  text: {
-    fontSize: 16,
-  },
-  subText: {
-    fontSize: 12,
-    color: 'gray',
   },
 });
